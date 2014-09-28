161242e6b23d578e2f58d66c6c24a0b462f19088
lillyfwang <lillyfwang@yahoo.com>
2014-09-28 15:55:05 -0400

    finished main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,26 @@
 $(document).ready(function(){
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-            if(this.hash.length > 3) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top-70
-            }, 900);
-            return false;
-          }
-        }
-    }
-  });
-
   $('#nav1').click(function() {
     $('html,body').animate({
       scrollTop: '0px'
-    }, 900);
+    }, 1500);
   });
 
   $('#nav2').click(function() {
     $('html,body').animate({
-      scrollTop: '2000px'
-    }, 900);
+      scrollTop: '2500px'
+    }, 1500);
   });
 
   $('#nav3').click(function() {
     $('html,body').animate({
-      scrollTop: '3250px'
-    }, 900);
+      scrollTop: '4600px'
+    }, 1500);
   });
 
   $('#nav4').click(function() {
     $('html,body').animate({
-      scrollTop: '4700px'
-    }, 900);
+      scrollTop: '9500px'
+    }, 1500)
   });
 
   $(function() {
